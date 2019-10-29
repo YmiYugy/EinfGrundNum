c3a193d5aeef69f4b7211c2568e77f6394c8a442
YmiYugy <ymi.yugy@gmx.de>
2019-10-29 01:33:34 +0100

    add the other parts

--- Aufgabe1/clenshaw.py
+++ Aufgabe1/clenshaw.py
@@ -1,6 +1,53 @@
-# Aufgabenteil c)
+import numpy as np
+import matplotlib.pyplot as plt
 
-def recursion_3term_iterative(F0,F1,beta,gamma,n,xs):
+# Aufgabenteil a)
+def clenshaw(alphas, xs):
+    # Input:
+    #   alphas  Liste / np.array mit den Koeffizienten der
+    #           Entwicklung in der Tschebyscheff Basis
+    #   xs      Liste / np.array mit den x-Werten an denen
+    #           ausgewertet werden soll
+    #
+    # Output:
+    #   ys      Liste mit den y-Werten
+
+    if not alphas:
+        raise ValueError("alphas must not be empty")
+    elif not alphas[:-1]:
+        return xs * alphas[0]
+    else:
+        def betas(alphas, x):
+            alpha0 = alphas[0]
+            beta2 = alphas[-1]
+            alphas = alphas[:-1]
+            beta1 = alphas[-1] + 2*x*beta2
+            while alphas:
+                tmp = alphas[-1] + 2*x*beta1 - beta2
+                alphas = alphas[:-1]
+                beta2 = beta1
+                beta1 = tmp
+            return alpha0 + x*beta1 - beta2
+        betasVec =  np.vectorize(lambda x : betas(alphas, x))
+        return betasVec(xs)
+
+
+# Aufgabenteil b)
+def plotChebyshev(n):
+    xs = np.linspace(-1, 1, num=200)
+    yss = [clenshaw([1 if i == j else 0 for i in range(j+1)], xs) for j in range(n)]
+    plt.title(f"Die ersten {n} Chebyshev-Polynome")
+    for i, ys in enumerate(yss):
+        plt.plot(xs, ys, label=f"n={i}")
+    plt.legend(loc="best")
+    plt.savefig("ChebyshevSample6.pdf", format="pdf")
+    plt.clf()
+
+plotChebyshev(6)
+
+
+# Aufgabenteil c)
+def recursion_3term_iterative(F0, F1, beta, gamma, n, xs):
     # Input:
     #   F0       lambda function, Startwert 0
     #   F1       lambda function, Startwert 1
@@ -11,10 +58,10 @@ def recursion_3term_iterative(F0,F1,beta,gamma,n,xs):
     # Output:
     #   ys       Liste der Werte Fn(x) fuer x in xs
     xs0 = [F0(x) for x in xs]
-    if n==0:
+    if n == 0:
         return xs0
     xs1 = [F1(x) for x in xs]
-    if n==1:
+    if n == 1:
         return xs1
     for i in range(n-1):
         xs2 = [beta * x1 + gamma * x0 for x0, x1 in zip(xs0, xs1)]
@@ -32,7 +79,7 @@ def pn_tschebyscheff_iterative(alphas, xs):
     #   ys          Funktionswerte an den Punkten x aus xs
 
     def tschebyscheff():
-        # Generiert die Tschebyscheff-Polynome ausgewertet an den Stellen xs        
+        # Generiert die Tschebyscheff-Polynome ausgewertet an den Stellen xs
         xs0 = [1 for x in xs]
         yield xs0
         xs1 = [x for x in xs]
@@ -47,3 +94,12 @@ def pn_tschebyscheff_iterative(alphas, xs):
     for ts, alpha in zip(tschebyscheff(), alphas):
         results = [r+alpha*t for r, t in zip(results, ts)]
     return results
+
+def chebyshevError(alphas, xs):
+    ys = abs(clenshaw(alphas, xs) - pn_tschebyscheff_iterative(alphas, xs))
+    plt.title("Absoluter Fehler bei der Auswertung des Polynoms")
+    plt.plot(xs, ys)
+    plt.savefig("Absoluter Fehler.pdf", format="pdf")
+    plt.clf()
+
+chebyshevError([0, 0, 2/3, 0, 4/14, 0, 23/96], np.linspace(0, 1, num=100))
