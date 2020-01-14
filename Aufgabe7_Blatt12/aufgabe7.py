bc1a990b3a90464c093085c3f3b16dd40f8398c8
Tristan Kobusch <tristan.kobusch@gmail.com>
2020-01-14 03:57:47 +0100

    this

--- Aufgabe7_Blatt12/aufgabe7.py
+++ Aufgabe7_Blatt12/aufgabe7.py
@@ -2,6 +2,7 @@ import numpy as np
 from scipy.linalg import block_diag
 import time
 from os import path
+from math import sqrt
 import urllib.request
 
 # a)
@@ -53,8 +54,56 @@ def compute_qrdecomp_householder(A):
     return (Q,R)
 
 # d)
+def compute_givens_rotation(A, i, j):
+    # Input
+    #   A       real matrix, n x n
+    #   i       row index
+    #   j       column index, i > j
+    # Output
+    #   G       Matrix of givens rotation for A_ij -> 0
+    if A.shape[0] != A.shape[1]:
+        raise ValueError("Must be square matrix")
+    elif j >= i:
+        raise ValueError("i must be greater than j")
+    a = float(A[j][j]/sqrt(A[j][j]**2 + A[i][j]**2))
+    
+    b = float(-A[i][j]/sqrt(A[j][j]**2 + A[i][j]**2))
+    Q = np.zeros(A.shape)
+    for x in range(A.shape[0]):
+        for y in range(A.shape[1]):
+            if x == y and x != i and y != j:
+                Q[x][y] = 1
+            elif (x == y and y == i) or (x == y and y == j):
+                Q[x][y] = a
+            elif x == i and y == j:
+                Q[x][y] = -b
+            elif x == j and y == i:
+                Q[x][y] = b
+            else:
+                Q[x][y] = 0
+    return Q
+
 
 # e)
+def compute_qrdecomp_givens(A):
+    # Input
+    #   A       real n x n matrix
+    # Output
+    #   Q       matrix Q from qr decomp
+    #   R       matrix R from qr decomp
+    gs = []
+    r = A
+    if A.shape[0] != A.shape[1]:
+        raise ValueError("Must be square matrix")
+    for j in range(A.shape[0]-1):
+        for i in range(j+1, A.shape[0]):
+            g = compute_givens_rotation(r, i, j)
+            gs.append(g)
+            r = g.dot(r)
+    Q = np.eye(A.shape[0])
+    for g in gs:
+        Q = Q.dot(g.T)
+    return Q, r
 
 # f)
 # Falls Matrix nicht im Verzeichnis liegt, von der Website runterladen.
@@ -80,3 +129,16 @@ print("||Q||₂ =", np.linalg.norm(q_h, ord=2))
 print("||Q*Qᵀ - I||F =", np.linalg.norm(q_h @ q_h.T - np.identity(A.shape[0])))
 print("||R - np.triu(R)||F =",np.linalg.norm(r_h-np.triu(r_h)))
 print("||Q*R - A||F =", np.linalg.norm(q_h@r_h-A))
+
+print("Givens-Rotation")
+
+start=time.time()
+q_g,r_g = compute_qrdecomp_givens(A)
+end = time.time()
+
+print("Ausführungsdauer: {:5.3f} Sekunden".format(end-start))
+print("Zur Kontrolle:")
+print("||Q||₂ =", np.linalg.norm(q_g, ord=2))
+print("||Q*Qᵀ - I||F =", np.linalg.norm(q_g @ q_g.T - np.identity(A.shape[0])))
+print("||R - np.triu(R)||F =",np.linalg.norm(r_g-np.triu(r_g)))
+print("||Q*R - A||F =", np.linalg.norm(q_g@r_g-A))
